5ed907f193e69365a85321b68bb02e4c73e99bfb
Etneuf <bfg10k@users.noreply.github.com>
2021-06-14 19:59:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,6 @@ chmod 600 "${HOME}/.ssh/id_rsa_deploy"
 rsync --version
 sh -c "
 rsync $2 \
-  -e 'ssh -i ${HOME}/.ssh/id_rsa_deploy -o StrictHostKeyChecking=no' \
   ${GITHUB_WORKSPACE}/public \
   ${VPS_DEPLOY_USER}@${VPS_DEPLOY_HOST}:${VPS_DEPLOY_DEST}
 "
